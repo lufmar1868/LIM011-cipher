3fde79036495e9ce32573748b44e9537b5b15448
lucyMartinez <lufmar1868@gmail.com>
2019-10-16 09:46:22 -0500

    subiendo trabajo

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,63 @@
+//let alfabeto = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p.q,r,s.t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,Q,P,Q,R,S,T,U,V,W,X,Y,Z";
+
+
+
 window.cipher = {
-  // Acá escribe tu código.
-};
+    encode: (string, offset) => {
+    // mensaje redactado a encriptar
+        let cifrado = "";
+        for (let i = 0; i < string.length; i++) {
+            let character = string[i];
+            
+                if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+                    let caracterTexto = (string.charCodeAt(i) - 65 + parseInt(offset)) % 26 + 65;
+                    cifrado += String.fromCharCode(caracterTexto);
+                    alert("vamos lucy tu puedes");
+                } else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
+                    caracterTexto = (string.charCodeAt(i) - 97 + parseInt(offset)) % 26 + 97;
+                    cifrado += String.fromCharCode(caracterTexto);
+                }
+              else {
+                cifrado += character;
+            }
+        }
+        return cifrado;
+    },
+    decode: (string, offset) => {
+        //mensaje recibido para desencriptar
+        let descifrado = "";
+
+        //inicio recorrido de caracteres
+        for (let i = 0; i < string.length; i++) {
+            let characterD = string[i];
+            //identificacion de espacios vacios
+            
+                if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+                    let caracterTexto = (string.charCodeAt(i) + 65 - parseInt(offset)) % 26 + 65;
+                    descifrado += String.fromCharCode(caracterTexto);
+                    alert("lucy lo estas logrando");
+                } else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
+                    let caracterTexto = ((string.charCodeAt(i) - 97 - parseInt(offset) + 52) % 26) + 97;
+                    descifrado += String.fromCharCode(caracterTexto);
+                }
+
+
+
+              else {
+                descifrado += characterD;
+            }
+        }
+        //retornar respuesta
+        return descifrado;
+    }
+}
+
+
+// let a =document.getElementById("msje");
+// undefined
+// a.value
+// "hoila"
+// let msjUsuario = a.value;
+// undefined
+// msjUsuario
+// "hoila"
---
+++ src/index.js
@@ -0,0 +1,60 @@
+// const cara1 = document.getElementById("iniciar");
+// const cara2 = document.getElementById("pag2");
+// const cara3 = document.getElementById("pag1");
+
+// cara1.addEventListener("click", ()=> {
+//     alert("buena")
+//     cara2.classList.remove("hide");
+//     alert("hola")
+//     cara1.classList.add("hide");
+//     alert("mira")
+// })
+
+document.getElementById("iniciar").addEventListener('click', ()=> {
+    alert("la hiciste")
+    document.getElementById('pag2').classList.remove('hide')
+    document.getElementById('pag1').classList.add('hide')
+})
+
+
+document.getElementById("btnredacta").addEventListener('click', ()=> {
+    alert("hola")
+    document.getElementById('pag3').classList.remove('hide');
+    document.getElementById('pag2').classList.add('hide');
+})
+
+
+document.getElementById("btnrecibe").addEventListener('click', ()=> {
+    alert("bien hecho")
+    document.getElementById('pag4').classList.remove('hide')
+    document.getElementById('pag2').classList.add('hide')
+})
+
+
+document.getElementById("offse1").addEventListener('click', ()=> {
+    alert("ya era hora")
+    document.getElementById("numOffset").classList.remove('hide')
+    document.getElementById('msje').classList.add('none')
+})
+
+codigo1.addEventListener("click",function(){
+    document.getElementById("codigo1").classList.add("codigo1");
+    document.getElementById("msjecodificado").classList.remove("display_none");
+    document.getElementById("msjecodificado").classList.add("display_block");
+})
+
+const botoncodificar = document.getElementById("offse1");
+botoncodificar.addEventListener("click", function() {
+    let codificar = document.getElementById("msje").value;
+    // textEncode = textEncode.toUpperCase();
+    let number_offset = document.getElementById("numOffset").value;
+    document.getElementById("msje1").innerHTML = window.cipher.encode(codificar, number_offset);
+});
+
+const botonDesencriptar = document.getElementById("btnDesencriptar");
+botonDesencriptar.addEventListener("click", function() {
+    let decodificar = document.getElementById("msjre").value;
+    //textDecode = textDecode.toUpperCase();
+    let number_offset = document.getElementById("numOffset1").value;
+    document.getElementById("desencriptado").innerHTML = window.cipher.decode(decodificar, number_offset);
+});
